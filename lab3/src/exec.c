3f8aed2d721279066530e7b01e101222fa91e8b4
Chinchilla Yura <razbakovyura@gmail.com>
2024-10-04 12:38:18 +0300

    3

--- lab3/src/exec.c
+++ lab3/src/exec.c
@@ -2,6 +2,6 @@
 #include <stdio.h>
 
 int main() {
-    execl("sequential_min_max", "sequential_min_max", "42", "10", NULL);
+    execl("sequential_min_max", "sequential_min_max", "42", "100", NULL);
     return 1;
 }
